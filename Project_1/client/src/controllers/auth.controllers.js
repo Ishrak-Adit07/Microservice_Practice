5b7387317ed559c6db692e8d0e8b54e35be8933d
Ishrak-Adit07 <ishrak.adit07@gmail.com>
2024-10-12 03:23:43 +0600

    Project1 client controllers update

--- Project_1/client/src/controllers/auth.controllers.js
+++ Project_1/client/src/controllers/auth.controllers.js
@@ -1,14 +1,21 @@
-const registerUser = async (name, password, confirmPassword) => {
-  if (!name || !password || !confirmPassword) {
-    throw Error("All fields are required");
+const validateFields = (fields) => {
+  for (const field of fields) {
+    if (!field) {
+      throw new Error("All fields are required");
+    }
   }
+};
+
+const registerUser = async (name, password, confirmPassword) => {
+
+  validateFields([name, password, confirmPassword]);
 
   if (password != confirmPassword) {
     throw Error("Passwords do not match");
   }
 
   try {
-    const registerResponse = await fetch("/api/user/register", {
+    const registerResponse = await fetch(`${import.meta.env.VITE_AUTH_API_URL}/api/user/register`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -33,12 +40,11 @@ const registerUser = async (name, password, confirmPassword) => {
 };
 
 const loginUser = async (name, password) => {
-  if (!name || !password) {
-    throw Error("All fields are required");
-  }
+
+  validateFields([name, password]);
 
   try {
-    const loginResponse = await fetch("/api/user/login", {
+    const loginResponse = await fetch(`${import.meta.env.VITE_AUTH_API_URL}/api/user/login`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",

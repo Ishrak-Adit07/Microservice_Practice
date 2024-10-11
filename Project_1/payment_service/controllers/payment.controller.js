93de3e5663bb4816fdcb2f0c2721eb5c26bae7e6
Ishrak-Adit07 <ishrak.adit07@gmail.com>
2024-10-12 01:22:45 +0600

    Services cleanup commit

--- Project_1/payment_service/controllers/payment.controller.js
+++ Project_1/payment_service/controllers/payment.controller.js
@@ -1,6 +1,4 @@
 import { Payment } from "../models/payment.model.js";
-import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
 import "dotenv/config.js";
 
 const makePayment = async (req, res) => {
@@ -10,11 +8,6 @@ const makePayment = async (req, res) => {
     if (!name || !payment) {
       return res.status(400).send({ error: "All fields are required" });
     }
-
-    // const exist = await Payment.findOne({ name });
-    // if (exist) {
-    //   return res.status(409).send({ error: "Name is already in use" });
-    // }
 
     const payment_done = await Payment.create({ name, payment });
 

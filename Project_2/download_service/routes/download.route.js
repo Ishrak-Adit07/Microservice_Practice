413d528b9a8bf32f8d7717374e69812fafc6de6d
Ishrak-Adit07 <ishrak.adit07@gmail.com>
2024-10-13 15:25:27 +0600

    Auth url fix with local dev fallback commit

--- Project_2/download_service/routes/download.route.js
+++ Project_2/download_service/routes/download.route.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { makeDownload } from "../controllers/download.controller.js";
+import auth from "../middlewares/auth.mw.js";
 const router = express.Router();
 
-router.post("/make", makeDownload);
-
+router.post("/make", auth, makeDownload);
 export default router;

566b1993d7c8febec8eabb4b8508192f9c3df570
Ishrak-Adit07 <ishrak.adit07@gmail.com>
2024-10-15 13:00:58 +0600

    Project 4 full copy commit

---
+++ Project_4/auth_service/app.js
@@ -0,0 +1,41 @@
+import express from "express";
+const app = express();
+
+// For parsing json
+app.use(express.json());
+
+// For cors
+import cors from "cors";
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  })
+);
+
+import mongoose from "mongoose";
+import { mongodbURL } from "./config.js";
+
+console.log("Trying to start mongodb");
+
+mongoose
+  .connect(mongodbURL, { dbName: "dfsa" })
+  .then(() => {
+    console.log("App connected to database");
+  })
+  .catch((err) => {
+    console.error("Failed to connect to MongoDB", err);
+    console.log(err);
+  });
+
+//Importing the routes
+import userRoute from "./routes/user.route.js";
+app.use("/api/user", userRoute);
+
+//Default URL
+app.use("/", (req, res) => {
+  res.send("Invalid URL!");
+});
+
+export default app;
